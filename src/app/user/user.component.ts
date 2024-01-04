ff9d3971a6aed4f09e3f5535893aa07bb5dafc11
tmazur <tomasz.mazur@onwelo.com>
2024-01-04 17:30:16 +0100

    add mapping for activity field

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserClient} from "../clients/user.client";
-import {Gender, User} from "../../User";
+import {Activity, Gender, User} from "../../User";
 import {UserInfoResponse} from "../../AuthenticationResponse";
 
 @Component({
@@ -8,45 +8,48 @@ import {UserInfoResponse} from "../../AuthenticationResponse";
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent implements OnInit{
+export class UserComponent implements OnInit {
 
   public userCredentials: User;
   public userInfo: UserInfoResponse;
-  public addInfo :boolean = false;
+  public addInfo: boolean = false;
   public editInfo: boolean = false;
 
   showAddInformationButton = true;
   showEditInformationButton = true;
+
   constructor(
-      private userClient:  UserClient,
-      ) {
+    private userClient: UserClient,
+  ) {
   }
 
   ngOnInit() {
-     this.userClient.getUserInfo().subscribe(userinfo => {
-       this.userInfo = userinfo
-       this.userClient.getUserCredentials(this.userInfo.id).subscribe(e => this.userCredentials = e)
-     });
+    this.userClient.getUserInfo().subscribe(userinfo => {
+      this.userInfo = userinfo
+      this.userClient.getUserCredentials(this.userInfo.id).subscribe(e => this.userCredentials = e)
+    });
   }
 
   openAddInformationForm() {
     this.addInfo = true;
     this.showAddInformationButton = false;
   }
+
   cancelInfo() {
     this.addInfo = false;
     this.showAddInformationButton = true;
-    this.showEditInformationButton= true;
-    this.editInfo= false;
+    this.showEditInformationButton = true;
+    this.editInfo = false;
   }
+
   saveInfo(data: any) {
-    this.userClient.createUserCredentials(this.userInfo.id, data).subscribe(e=> this.userCredentials=e);
-    this.addInfo= false;
+    this.userClient.createUserCredentials(this.userInfo.id, data).subscribe(e => this.userCredentials = e);
+    this.addInfo = false;
     this.showAddInformationButton = true;
   }
 
   openEditInformationForm() {
-    this.editInfo= true;
+    this.editInfo = true;
     this.showEditInformationButton = false;
   }
 
@@ -61,15 +64,35 @@ export class UserComponent implements OnInit{
     ['Female', Gender.FEMALE]
   ]);
 
+  activityListMap: Map<string, Activity> = new Map([
+    ['Sedentary', Activity.SEDENTARY],
+    ['Lightly active', Activity.LIGHTLY_ACTIVE],
+    ['Moderately active', Activity.MODERATELY_ACTIVE],
+    ['Very active', Activity.VERY_ACTIVE],
+    ['Extra active', Activity.EXTRA_ACTIVE]
+  ])
+
   formatGender() {
     let gender = this.userCredentials.gender;
     let genderKey: string;
-    for(const [k, v] of this.genderListMap) {
-      if(v === gender) {
+    for (const [k, v] of this.genderListMap) {
+      if (v === gender) {
         genderKey = k;
         break;
       }
     }
     return genderKey;
+  }
+
+  formatActivity() {
+    let activity = this.userCredentials.activity;
+    let activityKey: string;
+    for (const [k, v] of this.activityListMap) {
+      if (v === activity) {
+        activityKey = k;
+        break;
+      }
+    }
+    return activityKey;
   }
 }
