a3fb9a348cee8b9a1f4aea19025a0b09650166ef
tomekmazur95 <tomek.mazur95@gmail.com>
2024-05-14 23:51:51 +0200

    add routing for food dairy and my foods components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {authGuard} from "./helpers/auth.guard";
 import {LoginPageComponent} from "./login-page/login-page.component";
@@ -6,7 +6,8 @@ import {RegisterPageComponent} from "./register-page/register-page.component";
 import {UserComponent} from "./user/user.component";
 import {HomePageComponent} from "./home-page/home-page.component";
 import {GoalsComponent} from "./goals/goals.component";
-import {FoodComponent} from "./food/food.component";
+import {FoodDairyComponent} from "./food/food-dairy/food-dairy.component";
+import {MyFoodsComponent} from "./food/my-foods/my-foods.component";
 
 
 const routes: Routes = [
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path: 'profile', component: UserComponent, canActivate: [authGuard]},
   { path: '', component: HomePageComponent, canActivate: [authGuard]},
   { path: 'goals', component: GoalsComponent, canActivate: [authGuard]},
-  { path: 'food', component: FoodComponent, canActivate: [authGuard]}
+  { path: 'food/food_dairy', component: FoodDairyComponent, canActivate: [authGuard]},
+  { path: 'food/my_foods', component: MyFoodsComponent, canActivate: [authGuard]}
 ];
 @NgModule({
   imports: [
