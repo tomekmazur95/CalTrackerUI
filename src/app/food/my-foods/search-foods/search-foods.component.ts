871f1f413c8caa71a44f6b43cb0c4ccd9a803d52
tmazur <tomasz.mazur@onwelo.com>
2024-08-08 16:10:42 +0200

    add usage for findAll UserFoods

--- src/app/food/my-foods/search-foods/search-foods.component.ts
+++ src/app/food/my-foods/search-foods/search-foods.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FoodClient} from "../../../clients/food.client";
+import {UserClient} from "../../../clients/user.client";
+import {ResponseFoodDTO} from "../../../shared/models";
 
 @Component({
   selector: 'app-search-foods',
   templateUrl: './search-foods.component.html',
   styleUrl: './search-foods.component.css'
 })
-export class SearchFoodsComponent {
+export class SearchFoodsComponent implements OnInit {
 
+  userFoodList: ResponseFoodDTO[];
+  food: any;
+
+  constructor(private foodClient: FoodClient,
+              private userClient: UserClient) {
+  }
+
+  ngOnInit(): void {
+    this.userClient.getUserInfo().subscribe(userResponse => {
+      this.userClient.getUserCredentials(userResponse.id).subscribe(user => {
+        this.foodClient.findUserFoods(user.id).subscribe(foodList =>
+          this.userFoodList = foodList
+        )
+      })
+    })
+  }
 }
